b34964555a69f838985aff973ec60c40219cf393
18435139229 <1547205849@qq.com>
2016-07-22 22:35:35 +0800

    news
    news

---
+++ caches/caches_template/default/content/list - eight.php
@@ -0,0 +1,217 @@
+<?php defined('IN_PHPCMS') or exit('No permission resources.'); ?><!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>孤独是一座花园</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="<?php echo CSS_PATH;?>mycss/_init_.css" rel="stylesheet">
+    <link href="<?php echo CSS_PATH;?>mycss/post.css" rel="stylesheet">
+    <script src="<?php echo JS_PATH;?>myjs/ga.js" async="true"></script>
+</head>
+<body>
+    
+    <div class="bs-header">
+      <div class="container">
+        <ul class="header-download">
+          
+            <li><a href="" class="download-btn">下载iPhone版</a>
+          
+        </li></ul>
+        <div class="nav-logo">
+          <a class="logo" href="/">
+            <img width="48" height="48" src="<?php echo IMG_PATH;?>myimages/logo_56.png">
+          </a>
+          <a class="slogan" href="index.html">
+          一刻
+          <span>豆瓣每日内容精选</span>
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <div class="container bs-docs-container">
+      
+  <div class="post-container">
+    <div>
+      <h1 id="title">孤独是一座花园</h1>
+    </div>
+    <div class="meta">
+      
+      <img class="avatar" width="34" height="34" src="<?php echo IMG_PATH;?>myimages/u63525563-77.jpg">
+      
+    
+      <a href="">小馅儿</a>
+      的相册
+    
+      
+      
+    </div>
+    <div class="post-body">
+      <div id="content"><p>我的毕业创作:&gt;</p><div class="content_img"><img id="img_1" src="<?php echo IMG_PATH;?>myimages/338091.jpg"></div><div class="content_img"><img id="img_2" src="<?php echo IMG_PATH;?>myimages/338092.jpg"></div><div class="content_img"><img id="img_3" src="<?php echo IMG_PATH;?>myimages/338093.jpg"></div><div class="content_img"><img id="img_4" src="<?php echo IMG_PATH;?>myimages/338094.jpg"></div><div class="content_img"><img id="img_5" src="<?php echo IMG_PATH;?>myimages/338095.jpg"></div><div class="content_img"><img id="img_6" src="<?php echo IMG_PATH;?>myimages/338096.jpg"></div><div class="content_img"><img id="img_7" src="<?php echo IMG_PATH;?>myimages/338097.jpg"></div><div class="content_img"><img id="img_8" src="<?php echo IMG_PATH;?>myimages/338098.jpg"></div><div class="content_img"><img id="img_9" src="<?php echo IMG_PATH;?>myimages/338099.jpg"></div><div class="content_img"><img id="img_10" src="<?php echo IMG_PATH;?>myimages/338100.jpg"></div><div class="content_img"><img id="img_11" src="<?php echo IMG_PATH;?>myimages/367818.jpg"></div></div>
+      
+        
+      
+    </div>
+    <div class="post-ft">
+      
+        <a href="">查看原文&nbsp;&nbsp;</a><span>©&nbsp;版权属于作者&nbsp;&nbsp;商业转载联系作者</span>
+      
+      <div id="share">分享</div>
+    </div>
+    <div style="width: 1px; height: 1px; overflow: hidden;">
+      <img src="<?php echo IMG_PATH;?>myimages/logo_300.png">
+    </div>
+
+  </div>
+
+      
+    </div>
+
+    <div class="footer container">
+      
+  <div class="download">
+    <div class="qrcode">
+      <p>
+        扫描二维码下载「一刻」
+        <br>豆瓣每日内容精选
+        <br><span>支持&nbsp;iPhone&nbsp;/&nbsp;Android&nbsp;/&nbsp;WP</span>
+      </p>
+      
+        <img width="130" height="130" src="<?php echo IMG_PATH;?>myimages/81d4856921ea4cb532daa3bd756a98c5.jpg">
+      
+    </div>
+    <div class="download-links">
+      <a href="" class="prestoicon-apple">iPhone下载</a>
+      <a href="" class="prestoicon-android">Android下载</a>
+      <a href="" class="prestoicon-windows8">WP下载</a>
+    </div>
+  </div>
+
+  <div id="comment">
+      <h2>文章评论（3）</h2>
+      
+      <div class="comment-login">
+          <a href="">请<span>登录</span>后写下你的评论</a>
+      </div>
+      
+
+      
+      <ul class="comment-list">
+          
+<li>
+    <div class="user">
+        
+        <img src="<?php echo IMG_PATH;?>myimages/u89990547-4.jpg" alt="zov" width="42" height="42">
+        
+    </div>
+    <div class="content">
+        <div class="meta">
+            <span class="time" title="2016-07-22 10:29:34">10:29</span>
+            
+            zov
+            
+        </div>
+        哎呀，之前在广美本科展上看见就挺喜欢的。
+        
+    </div>
+</li>
+
+<li>
+    <div class="user">
+        
+        <img src="<?php echo IMG_PATH;?>myimages/u35009195-1.jpg" alt="麦兜" width="42" height="42">
+        
+    </div>
+    <div class="content">
+        <div class="meta">
+            <span class="time" title="2016-07-22 03:15:55">03:15</span>
+            
+            麦兜
+            
+        </div>
+        真的只有一棵树么～
+        
+    </div>
+</li>
+
+<li>
+    <div class="user">
+        
+        <img src="<?php echo IMG_PATH;?>myimages/u56685137-4.jpg" alt="渠野" width="42" height="42">
+        
+    </div>
+    <div class="content">
+        <div class="meta">
+            <span class="time" title="2016-07-22 02:46:02">02:46</span>
+            
+            渠野
+            
+        </div>
+        是手绘的还是板绘的 还是手绘扫描进电脑的
+        
+    </div>
+</li>
+
+      </ul>
+      
+      
+
+      
+      <p class="more-comment">
+          更多评论及精选内容<br>
+          请下载「一刻」App查看
+      </p>
+      
+      <div class="random-post">
+        <a href="">
+          <h4>生活在恐袭阴影下是怎样的体验——伪球迷的欧洲杯手记</h4>
+          
+            
+              <img class="pic" src="<?php echo IMG_PATH;?>myimages/367608.jpg">
+            
+          
+          <div class="abstract">那些恐惧，那些悲伤，都在热闹的气氛中一点点消散。音乐声和欢呼声给了这个千疮百孔的国家安慰，给了她生活下去的勇气和希望。</div>
+        </a>
+      </div>
+      
+      <a href="" class="app-download">更多精彩内容，下载豆瓣一刻客户端</a>
+      
+
+      <div id="sideNav" style="display: none;">
+          <a href="" id="backToTop"><b class="arrow"></b>回顶部</a>
+      </div>
+
+      
+  </div>
+
+  <div class="bs-header" style="display:block;">
+      <div class="container">
+        <ul class="header-download">
+          
+            <li><a href="" class="download-btn">下载iPhone版</a>
+          
+        </li></ul>
+        <div class="nav-logo">
+          <a class="logo" href="">
+            <img width="48" height="48" src="<?php echo IMG_PATH;?>myimages/logo_56.png">
+          </a>
+          <a class="slogan" href="">
+          一刻
+          <span>豆瓣每日内容精选</span>
+          </a>
+        </div>
+      </div>
+    </div>
+
+
+      <p class="copyright">
+        ©&nbsp;2005－2016&nbsp;douban.com, all rights reserved<br>
+        北京豆网科技有限公司
+      </p>
+    </div>
+    <script src="<?php echo JS_PATH;?>myjs/jquery.js"></script>
+    <link type="text/css" rel="stylesheet" href="<?php echo CSS_PATH;?>mycss/dialog.css">
+    <script type="text/javascript" src="<?php echo JS_PATH;?>myjs/dialog.js" async="true"></script>
+    <script src="<?php echo JS_PATH;?>myjs/_init_.js"></script>
+</body>
+</html>
