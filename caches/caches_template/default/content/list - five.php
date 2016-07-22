de1684ed474e49f87fe881ba8cff32bc508f66fe
18435139229 <1547205849@qq.com>
2016-07-22 18:41:30 +0800

    news
    cxcsd

---
+++ caches/caches_template/default/content/list - five.php
@@ -0,0 +1,289 @@
+<?php defined('IN_PHPCMS') or exit('No permission resources.'); ?><!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>整形</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="<?php echo CSS_PATH;?>mycss/_init_.css" rel="stylesheet">  
+  <link href="<?php echo CSS_PATH;?>mycss/post.css" rel="stylesheet">
+  <script src="<?php echo JS_PATH;?>myjs/ga.js" async="true"></script>
+</head>
+<body>
+    
+    <div class="bs-header">
+      <div class="container">
+        <ul class="header-download">
+          
+            <li><a href="" class="download-btn">下载iPhone版</a>
+          
+        </li></ul>
+        <div class="nav-logo">
+          <a class="logo" href="/">
+            <img width="48" height="48" src="<?php echo IMG_PATH;?>myimages/logo_56.png">
+          </a>
+          <a class="slogan" href="index.html">
+          一刻
+          <span>豆瓣每日内容精选</span>
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <div class="container bs-docs-container">
+      
+  <div class="post-container">
+    <div>
+      <h1 id="title">整形</h1>
+    </div>
+    <div class="meta">
+      
+      <img class="avatar" width="34" height="34" src="<?php echo IMG_PATH;?>myimages/u41952166-15.jpg">
+      
+    
+      <a href="">魏庆</a>
+      的日记
+    
+      
+      
+    </div>
+    <div class="post-body">
+      <div id="content"><p>“你过来。”</p><p>我朝背对着我的助理挥挥手。</p><p>她没动。似乎正全神贯注地瞧着墙上玻璃框里的抽象画。</p><p>我再次张嘴，想加大音量，才意识到自己的喉咙只是干巴巴摩擦了几下，没发出一点声音。伴着密集的刺痛，我的脸肿得厉害，舌头在口腔里几乎难以伸展，嘴唇也肿得老高，一下子感觉嘴巴大了许多。</p><p>私人医院的病房装修得像高级酒店，无论灯光布置还是家具选择，都让人有种想躺下休息的安心感。我做了能使皮肤变白变嫩的“黑脸娃娃”，嘴唇、下巴、上眼皮注射了玻尿酸，更换了几年前植入的鼻子假体，重新做了鼻头。眼睛的话，以前做了埋线双眼皮，现在已经变得很淡，眼皮稍微下垂就能看到断线一样的印记，像指甲盖上脱落了一半的指甲油显得人很潦草。但医生说下次再做，那就下次吧。</p><p>我侧头看了眼窗外，天色已经全黑了。</p><p>中午叫助理陪我来诊室，本想只是咨询一下，没想到相熟的医生说“过阵子都会很忙，恐怕不在国内。择日不如撞日，不如就今天做吧。下午我正好有空。”几句话我就缠上了纱布。</p><p>一年前，我有了自己的贴身助理。</p><p>如果知道自己有一天会在公共场合抛头露脸，我就不会那么早结婚了吧。一起做事的同行曾在背后说，“她看男人的眼光太差了，他像她的司机。”，这话传到了我的耳朵里。我感到很丢脸，受到羞辱般。</p><p>让我感到羞辱的，不是那些在背后嚼舌根的人，而是没本事的老公。</p><p>但……老公也曾“有本事”吧。</p><p>我从一个三流大学毕业后找不到工作，借打游戏逃避现实，最落魄时在游戏战场上遇到老公。我们很快见面，连房租也付不起的我很快搬进老公的住处。我对数字计算、流行科技、电脑技巧、任何一门外语，所有牵扯到“理性”的东西，统统一窍不通，甚至银行卡开通网络支付也让我焦头烂额。老公简直是我的救星，他帮我处理这一切。</p><p>那之后不久，我的个人职业初露曙光。什么也不懂的我唯一的优点就是爱看书。书看多了，就有了写的欲望。一开始是在网上写游戏战术，后来写恐怖小说，再后来学着人家写时评，慢慢有了一票追捧的人，竟渐渐有了名气，接着有人找我约稿。</p><p>现在回想这一切，老公不光帮我挡住只要活着就需要应付的客观世界，也在客观世界之外给了我从未体会过的柔情。包括深夜里他用尽全力进入我的身体，都曾是最让我感到安心的部分。</p><p>老公相貌平平，工作平平，我也姿色平平，身材平平，我曾想，“这就是两个平平的人的结合了”——但也许正因为心底深埋着的某种野心，才会这样劝说自己吧。</p><p>现在几年过去了。老公还在原来的公司，做着同样的工作，薪水有了小幅度增长，职位上了一个轻轻的台阶。我四处接稿，录电台，做电视嘉宾，出书做编剧，我的收入翻了好多倍。</p><p>当我遇到职业问题，不能给出意见甚至没有见解的老公，收入没我多的老公，不高也不帅的老公，对我百依百顺的老公，我现在，一点点都喜欢不起来。我甚至……瞧不起他。</p><p>“脸好多了。没那么肿了。换一个。”</p><p>助理不知道什么时候站到了我面前，递给我新的冰袋。我接过来，重新贴到脸上，一股沁凉由脸颊直传到心底。</p><p>我让助理拿镜子过来。</p><p>脸肿得几乎将眼睛挤没了，又肿又亮，我用手指轻轻一摁，留下白白的指印，过了好一会儿，指印才完全消失。满脸细密的针孔，针孔朝内凹进去，仿佛什么虫子钻了进去，又会随时爬出来。</p><p>我扔掉镜子。</p><p>过一会儿又拿起镜子。</p><p>“你说下巴是不是打多了？”</p><p>助理仔细看了一会儿，“应该还好。医生不是说过几天会吸收一些嘛，两周左右就特别自然了。”</p><p>“眼皮这儿呢？”</p><p>“也还好。我觉得。”</p><p>“你看是不是两边打得不一样多？”</p><p>“……好像有一点诶。”</p><p>我心里默默骂了一句，使劲眨眼，看看是不是因为脸肿才显得不对称。</p><p>“吸收几天再看吧。差别还大的话，再过来让医生看看。”助理说。</p><p>我点点头，看着镜子里自己的鼻子。这一次的假体比之前纤细多了，加上小巧的鼻头，我觉得自己整个人变秀气了。</p><p>我不禁想起自己第一次做鼻子的事。</p><p>那时我刚出完第一本书，录了自己的第一档节目。本期待赞美，没想到收来的全是嘲讽。网友讽刺我是“大妈脸”，我这才意识到自己就是常说的那种“塌鼻子”。没有鼻子，加上很差的皮肤状况，我的脸上镜之后毫无生气，甚至有种哭丧感。</p><p>那时整形远没现在这样流行，但我知道这关系到我的职业生涯。手术做完回到老家，母亲发现我变样了。我老实告知，母亲大为光火。</p><p>“你要做文化，你就好好做文化，你想出名，就去好好学本事。你肚子里要是有东西，别人迟早会看得到。”</p><p>“时代早不是你想的那样了！没有脸，谁有兴趣看你肚子里装的是什么。再说，肚里有东西的人多的是，人家为什么不去看漂亮的脸。脸都不能让人愉悦，谁相信你里面有什么能愉悦人的东西！要是你把我生好看点，我用得着花钱挨刀去做这些吗！”</p><p>我们就此大吵一架。</p><p>到现在母亲都没原谅我，我们之间的话越来越少。父亲倒是很宽容，“你觉得那样有帮助，你就做吧。”母亲冥顽不灵，稍微一生气，就骂我虚荣。每年过年我只象征性的回家待几天，能躲就躲，总觉得离那个家越来越远。上电视，出名，对母亲来说遥远得不真实，我们已经不在一个世界了。</p><p>“天已经全黑了。”</p><p>助理从门外进来。</p><p>我点点头。</p><p>“这一排只有我们这个房间的灯亮着。”</p><p>“我们也走吧。”我看看表已经十点多了。</p><p>助理拿来口罩，又扶我下床，双脚着地的时候我突然一软，身体一个打晃。助理慌忙扶住我。</p><p>“没事吧？”</p><p>“没事。”</p><p>整个楼道一片黑暗，电梯叮咚停住，才有一小片亮光射出来。</p><p>一楼大厅的灯也全熄了，只有一侧护士值班室亮着灯。助理拎着东西走在前面，脚步声空荡荡地四下散开。我不由紧走几步，追上助理，一把抓住她的胳膊。</p><p>助理没有回头，带着我默默朝前走。我的余光扫到大厅两侧写着“安全出口”的绿莹莹的标牌，但我们此刻正朝相反的方向走去。</p><p>车子停在路边，待我坐到副驾，助理熟练地发动车子。一切迎面而来，一切又被甩到身后。突然下起了雨，我突然想哭，眼泪接着掉下来。</p><p>到了楼下，助理递给我一个塑料袋，叮嘱我什么药什么时候吃，告诉我记不住也没关系她到时间会提醒我。她给我第二个塑料袋，告诉我待会回去放冰箱，明天再冰敷一天。助理转身上车，我朝她挥挥手。</p><p>老公曾帮我做的一切，助理正帮我完成着。 </p><p>我进门的时候，老公正从洗衣机里掏出一大团衣服。他没有看我，穿过我，径直走到阳台。</p><p>直到睡觉，我们没说一句话。老公也没看我一眼。我们躺在床的两边，听着对方的呼吸。过一会儿，老公靠过来，把头轻轻地放在了我的肩膀上。</p><p>———</p><p>未收入《二十八岁，我有点累》的同系列故事。</p></div>
+      
+        
+      
+    </div>
+    <div class="post-ft">
+      
+        <a href="">查看原文&nbsp;&nbsp;</a><span>©&nbsp;版权属于作者&nbsp;&nbsp;商业转载联系作者</span>
+      
+      <div id="share">分享</div>
+    </div>
+    <div style="width: 1px; height: 1px; overflow: hidden;">
+      <img src="<?php echo IMG_PATH;?>myimages/logo_300.png">
+    </div>
+
+  </div>
+
+      
+    </div>
+
+    <div class="footer container">
+      
+  <div class="download">
+    <div class="qrcode">
+      <p>
+        扫描二维码下载「一刻」
+        <br>豆瓣每日内容精选
+        <br><span>支持&nbsp;iPhone&nbsp;/&nbsp;Android&nbsp;/&nbsp;WP</span>
+      </p>
+      
+        <img width="130" height="130" src="<?php echo IMG_PATH;?>myimages/d4e36573430c86f8eaad46d1ca381b54.jpg">
+      
+    </div>
+    <div class="download-links">
+      <a href="" class="prestoicon-apple">iPhone下载</a>
+      <a href="" class="prestoicon-android">Android下载</a>
+      <a href="" class="prestoicon-windows8">WP下载</a>
+    </div>
+  </div>
+
+  <div id="comment">
+      <h2>文章评论（13）</h2>
+      
+      <div class="comment-login">
+          <a href="">请<span>登录</span>后写下你的评论</a>
+      </div>
+      
+
+      
+      <ul class="comment-list">
+          
+<li>
+    <div class="user">
+        
+        <img src="<?php echo IMG_PATH;?>myimages/u101916614-2.jpg" alt="屁股大大" width="42" height="42">
+        
+    </div>
+    <div class="content">
+        <div class="meta">
+            <span class="time" title="2016-07-22 11:40:51">21分钟前</span>
+            
+            屁股大大
+            
+        </div>
+        这是啥，没头没尾
+        
+    </div>
+</li>
+
+<li>
+    <div class="user">
+        
+        <img src="<?php echo IMG_PATH;?>myimages/u126049766-7.jpg" alt="肥喵喵callie" width="42" height="42">
+        
+    </div>
+    <div class="content">
+        <div class="meta">
+            <span class="time" title="2016-07-22 10:19:59">10:19</span>
+            
+            肥喵喵callie
+            
+        </div>
+        哈？没看明白⋯⋯
+        
+    </div>
+</li>
+
+<li>
+    <div class="user">
+        
+        <img src="<?php echo IMG_PATH;?>myimages/u90280311-1.jpg" alt="顺风" width="42" height="42">
+        
+    </div>
+    <div class="content">
+        <div class="meta">
+            <span class="time" title="2016-07-22 09:16:44">09:16</span>
+            
+            顺风
+            
+        </div>
+        是从身边穿过，你太会联想了
+        
+    </div>
+</li>
+
+<li>
+    <div class="user">
+        
+        <img src="<?php echo IMG_PATH;?>myimages/u145185233-1.jpg" alt="午后晒太阳的猫" width="42" height="42">
+        
+    </div>
+    <div class="content">
+        <div class="meta">
+            <span class="time" title="2016-07-22 09:14:27">09:14</span>
+            
+            午后晒太阳的猫
+            
+        </div>
+        恐怖故事？老公“穿过”我，难道已经死了？
+        
+    </div>
+</li>
+
+<li>
+    <div class="user">
+        
+        <img src="<?php echo IMG_PATH;?>myimages/u130050062-2.jpg" alt="夏天的风" width="42" height="42">
+        
+    </div>
+    <div class="content">
+        <div class="meta">
+            <span class="time" title="2016-07-22 08:52:22">08:52</span>
+            
+            夏天的风
+            
+        </div>
+        毕竟看脸只是一时，更多还是肚子里的东西
+        
+    </div>
+</li>
+
+<li>
+    <div class="user">
+        
+        <img src="<?php echo IMG_PATH;?>myimages/u98563837-2.jpg" alt="bonnie" width="42" height="42">
+        
+    </div>
+    <div class="content">
+        <div class="meta">
+            <span class="time" title="2016-07-22 08:42:14">08:42</span>
+            
+            bonnie
+            
+        </div>
+        有生活
+        
+    </div>
+</li>
+
+<li>
+    <div class="user">
+        
+        <img src="<?php echo IMG_PATH;?>myimages/user_normal.jpg" alt="张" width="42" height="42">
+        
+    </div>
+    <div class="content">
+        <div class="meta">
+            <span class="time" title="2016-07-22 08:29:50">08:29</span>
+            
+            张
+            
+        </div>
+        没了吗。有没有连载
+        
+    </div>
+</li>
+
+      </ul>
+      
+      
+
+      
+      <p class="more-comment">
+          更多评论及精选内容<br>
+          请下载「一刻」App查看
+      </p>
+      
+      <div class="random-post">
+        <a href="">
+          <h4>孤独是一座花园</h4>
+          
+            
+              <img class="pic" src="<?php echo IMG_PATH;?>myimages/338091.jpg">
+            
+          
+          <div class="abstract">我的毕业创作</div>
+        </a>
+      </div>
+      
+      <a href="" class="app-download">更多精彩内容，下载豆瓣一刻客户端</a>
+      
+
+      <div id="sideNav" style="display: none;">
+          <a href="" id="backToTop"><b class="arrow"></b>回顶部</a>
+      </div>
+
+      
+  </div>
+
+  <div class="bs-header" style="display:block;">
+      <div class="container">
+        <ul class="header-download">
+          
+            <li><a href="" class="download-btn">下载iPhone版</a>
+          
+        </li></ul>
+        <div class="nav-logo">
+          <a class="logo" href="">
+            <img width="48" height="48" src="<?php echo IMG_PATH;?>myimages/logo_56.png">
+          </a>
+          <a class="slogan" href="">
+          一刻
+          <span>豆瓣每日内容精选</span>
+          </a>
+        </div>
+      </div>
+    </div>
+
+
+      <p class="copyright">
+        ©&nbsp;2005－2016&nbsp;douban.com, all rights reserved<br>
+        北京豆网科技有限公司
+      </p>
+    </div>
+    <script src="<?php echo JS_PATH;?>myjs/jquery.js"></script>
+    <link type="text/css" rel="stylesheet" href="<?php echo CSS_PATH;?>mycss/dialog.css">
+    <script type="text/javascript" src="<?php echo JS_PATH;?>myjs/dialog.js" async="true"></script>
+    <script src="<?php echo JS_PATH;?>myjs/_init_.js"></script>
+</body>
+</html>
